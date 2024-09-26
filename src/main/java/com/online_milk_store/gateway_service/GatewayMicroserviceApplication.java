bcfe616b3905d4fe6ba8945d9663099ef19b6ecb
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-26 09:02:56 +0530

    initial commit

---
+++ src/main/java/com/online_milk_store/gateway_service/GatewayMicroserviceApplication.java
@@ -0,0 +1,15 @@
+package com.online_milk_store.gateway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayMicroserviceApplication.class, args);
+	}
+
+}
