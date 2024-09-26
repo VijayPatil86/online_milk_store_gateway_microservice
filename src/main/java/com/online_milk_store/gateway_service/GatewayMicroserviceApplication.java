72c9ab0fba2ce4f6910cd3a472cc7b899a42da7a
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-26 14:49:14 +0530

    added annotation @CrossOrigin

--- src/main/java/com/online_milk_store/gateway_service/GatewayMicroserviceApplication.java
+++ src/main/java/com/online_milk_store/gateway_service/GatewayMicroserviceApplication.java
@@ -3,7 +3,9 @@ package com.online_milk_store.gateway_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayMicroserviceApplication {
